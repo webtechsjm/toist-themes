ae0526deb20b75006226a2115c56b2bfe5e8486d
Senning Luk <sluk@stage.torontoist.stjosephmedia.com>
2013-02-19 20:11:02 +0000

    Updated client services TODO

--- torontoist_theme20112/torontoistTODO.php
+++ torontoist_theme20112/torontoistTODO.php
@@ -33,85 +33,49 @@ $(function() {
     <area shape="rect" coords="460,46,598,65" href="http://www.torontolife.com/TOdo_sales/" alt="Add Your Event" />
   </map>
 </div>
-<!--soledad-->
+<!--eps-->
 <div class="todo_container">
-  <img src="http://media.torontolife.com/img/TOdo/Flamenca_600x100.jpg" width="600px" height="100px" />
+  <img src="http://media.torontolife.com/img/TOdo/study_go_abroad_todo2013_600x100.jpg" width="600px" height="100px" />
   <p style="margin-top: 5px; margin-bottom: 10px;" class="expand">Details</p>
   <div class="collapse">
-    <img class="product" src="http://media.torontolife.com/img/TOdo/Flamenca_260x260.jpg" width="260px" height="260px" />
-    <div class="info"><p><b>Soledad Barrio and Noche Flamenca </b><br/>
-    <b>Friday, January 18 & Saturday, January 19, 2013 8pm </b><br/>
-    <b>Koerner Hall, TELUS Centre for Performance and Learning, 273 Bloor Street West</b><br/>
-    <b>Tickets: 416-408-0208 or <strong><a style="color:#000;" href="http://www.performance.rcmusic.ca" target="_blank">www.performance.rcmusic.ca</a>
+    <img class="product" src="http://media.torontolife.com/img/TOdo/study_go_abroad_todo2013_260x260.jpg" width="260px" height="260px" />
+    <div class="info">
+    <p><b>Don't miss the most important event of the year for studies or travel abroad</b><br/>
+    Study and Go Abroad student expo<br/>
+Sunday, March 3 – Metro Toronto Convention Centre, North Building<br/><br/>
+
+    Don't miss the Study and Go Abroad Fair, Canada's largest international university and student travel expo.   The expo, which is open from 1pm to 5pm on Sunday, March 3rd, offers visitors an exciting opportunity to meet with top-ranked universities and colleges from around the world to learn about undergraduate, post-graduate,  vocational and language programs abroad.  Our student travel pavilion will showcase a range of gap year options, work abroad programs, volunteering and adventure travel.  <br/><br/> Come an hour before the fair opens to catch our guest speaker seminar (check our website for more information).<br/><br/>
+    Pre-register online or on the day and you will be entered into the Grand Prize Draw.<br/>
+
+Admission and seminars are FREE.<br/><br/>
+
+  <strong><a style="color:#000;" href="http://www.torontolife.com/revolveepsholdings_todo" target="_blank">Click here for more info &raquo;</a></strong></p>
+    </div>
+    <img src="http://media.torontolife.com/hotdeals/images/underline.gif" width="600px" height="20px" />
+  </div>
+</div>
+<!--end eps-->
+<!--ballet jorgen-->
+<div class="todo_container">
+  <img src="http://media.torontolife.com/img/TOdo/swan-lake_600x100.jpg" width="600px" height="100px" />
+  <p style="margin-top: 5px; margin-bottom: 10px;" class="expand">Details</p>
+  <div class="collapse">
+    <img class="product" src="http://media.torontolife.com/img/TOdo/swan-lake_260x260.jpg" width="260px" height="260px" />
+    <div class="info"><p><b>SWAN LAKE</b><br/>
+    <b>March 27, 28, &amp; 30, 2013</b><br/>
+    <b>Betty Oliphant Theatre</b><br/>
+    <b>Tickets: 416.978.8849 or <strong><a style="color:#000;" href="http://www.uofttix.ca" target="_blank">www.uofttix.ca</strong></a>
 </b><br/>
 <br/>
-    The Royal Conservatory presents the return of Noche Flamenca. Under the direction of Martín Santangelo, with lead dancer Soledad Barrio, the award-winning Noche Flamenca "creates such a soulful world, by turns tender and ferocious." (The New York Times) <br /> 
-Tickets start at only $35 (including HST and service charge).
+   Ballet J&ouml;rgen Canada presents a spectacular new production of Swan Lake. Choreographed by Bengt Jörgen, this newly-imagined version of the iconic ballet is set at the Fortress of Louisbourg, Nova Scotia.<br /> 
+Tickets $29 - $75.
 <br/><br/>
-  <strong><a style="color:#000;" href="http://www.torontolife.com/royalconservatory_nocheflamenca_todo_torontoist" target="_blank">Click here for more info &raquo;</a></strong></p>
+  <strong><a style="color:#000;" href="http://www.torontolife.com/balletjorgencanada_swanlake_todo" target="_blank">Click here for more info &raquo;</a></strong></p>
     </div>
     <img src="http://media.torontolife.com/hotdeals/images/underline.gif" width="600px" height="20px" />
   </div>
 </div>
-<!--end soledad-->
-<!--jens-->
-<div class="todo_container">
-  <img src="http://media.torontolife.com/img/TOdo/jensbanner__600x100.jpg" width="600px" height="100px" />
-  <p style="margin-top: 5px; margin-bottom: 10px;" class="expand">Details</p>
-  <div class="collapse">
-    <img class="product" src="http://media.torontolife.com/img/TOdo/jenslindemann_260x260.jpg" width="260px" height="260px" />
-    <div class="info"><p><b>Jens Lindemann with Doc Severinsen and Wycliffe Gordon</b><br/>
-    <b>Saturday, January 12, 2012 8pm</b><br/>
-    <b>Koerner Hall, TELUS Centre for Performance and Learning, 273 Bloor Street West</b><br/>
-    <b>Tickets: 416-408-0208 or <strong><a style="color:#000;" href="http://www.performance.rcmusic.ca" target="_blank">www.performance.rcmusic.ca</a>
-</b><br/>
-<br/>
-    The Royal Conservatory presents four of North America's top horn players – International Brass Personality of the Year trumpeter Jens Lindemann, former Tonight Show bandleader Doc Severinsen, and six-time Trombonist of the Year Wycliffe Gordon- with a Toronto all-star ensemble.<br /> 
-Tickets start at only $45 (including HST and service charge).
-<br/><br/>
-  <strong><a style="color:#000;" href="http://www.torontolife.com/royalconservatory_jenslindemann_todo_torontoist" target="_blank">Click here for more info &raquo;</a></strong></p>
-    </div>
-    <img src="http://media.torontolife.com/hotdeals/images/underline.gif" width="600px" height="20px" />
-  </div>
-</div>
-<!--end jens-->
-
-
-<!--world lit-->
-<div class="todo_container">
-  <img src="http://media.torontolife.com/img/TOdo/Kama2013-BannerAd-600x100.jpg" width="600px" height="100px" />
-  <p style="margin-top: 5px; margin-bottom: 10px;" class="expand">Details</p>
-  <div class="collapse">
-    <img class="product" src="http://media.torontolife.com/img/TOdo/Kama2013-Ad-260x260.jpg" width="260px" height="260px" />
-    <div class="info"><p><b>World Literacy Canada’s 21st KAMA Benefit Reading Series</b><br/>
-    Join us for cocktails, hors d’oeuvres, readings, and great conversation.<br/>
-This year’s lineup includes Yann Martel, Elizabeth May, Carol Off, and more!<br/>
-Readings take place monthly at the Park Hyatt Toronto, January to May.
-Visit <a href="http://www.torontolife.com/worldliteracycanada_todo_torontoist">worldlit.ca/kama</a> or call 416.977.0008 for more info.
-
-<br/><br/>
-<strong><a style="color:#000;" href="http://www.torontolife.com/worldliteracycanada_todo_torontoist" target="_blank">Click here for more info &raquo;</a></strong></p>
-    </div>
-    <img src="http://media.torontolife.com/hotdeals/images/underline.gif" width="600px" height="20px" />
-  </div>
-</div>
-<!--world lit-->
-
-<!--start gardiner-->
-<div class="todo_container">
-  <img src="http://media.torontolife.com/img/TOdo/GardinerVase_banner_2012.gif" width="600px" height="100px" />
-  <p style="margin-top: 5px; margin-bottom: 10px;" class="expand">Details</p>
-  <div class="collapse">
-    <img class="product" src="http://media.torontolife.com/img/TOdo/GardinerVase_main2012.gif" width="260px" height="260px" />
-    <div class="info"><p><b>October 11, 2012 – January 13, 2013</b><br/><br/>
-    Celebrating the art of copying and the role of the usually anonymous artist in Jingdezhen, China, The Vase Project presents a visual chain letter of 101 hand-painted blue-and-white vases copied and based on an original sketch of the smoke stacks of Jingdezhen.
-    <br/><br/>
-<strong><a style="color:#000;" href="http://www.torontolife.com/gardinermuseum_todo_torontoist" target="_blank">Click here for more info &raquo;</a></strong></p>
-    </div>
-    <img src="http://media.torontolife.com/hotdeals/images/underline.gif" width="600px" height="20px" />
-  </div>
-</div>
-<!--end gardiner-->
+<!--end ballet jorgen-->
 </div>
 
 <?php get_sidebar(); ?>
