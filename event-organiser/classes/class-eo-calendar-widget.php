9e053aa0a9e4924bb0f9f359bcc942fd6b679240
Senning Luk <sluk@stage.torontoist.stjosephmedia.com>
2013-03-06 00:17:11 +0000

    Calendar fix

--- event-organiser/classes/class-eo-calendar-widget.php
+++ event-organiser/classes/class-eo-calendar-widget.php
@@ -123,12 +123,13 @@ class EO_Calendar_Widget extends WP_Widget
 * param $month - DateTime object for first day of the month (in blog timezone)
 */
 function generate_output($month,$args=array()){
-
+	/*
 	$key= $month->format('YM').serialize($args).get_locale();
 	$calendar = get_transient('eo_widget_calendar');
 	if( $calendar && is_array($calendar) && isset($calendar[$key]) ){
 		return $calendar[$key];
 	}
+	*/
 	
 	//Translations
 	global $wp_locale;
@@ -256,7 +257,7 @@ function generate_output($month,$args=array()){
 	
 	$calendar[$key] = $before.$title.$head.$foot.$body.$after;
 
-	set_transient('eo_widget_calendar',$calendar, 60*60*24);
+	set_transient('eo_widget_calendar',$calendar, 60*60);
 	return $calendar[$key];
 	}
 }
