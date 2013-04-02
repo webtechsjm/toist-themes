37b3d0fa7ded2205b11cace15dfe5257c5d56e1d
Senning Luk <sluk@stage.torontoist.stjosephmedia.com>
2013-04-02 23:06:40 +0000

    removes feature_tag directly from the table

--- toist-feature-fixer2.php
+++ toist-feature-fixer2.php
@@ -5,7 +5,7 @@
 Plugin Name: Torontoist Featured Tag fixer
 Plugin URI: http://torontoist.com
 Description: Finds posts with the feature_tag postmeta and changes them to featured_tag
-Version: 0.1
+Version: 0.2
 Author: Senning Luk
 Author URI: http://puppydogtales.ca
 License: GPLv2 or later
@@ -46,6 +46,24 @@ function feature_fix_page(){
 	
 	printf("<strong>%d posts fixed</strong>",
 		count($posts->posts)
+	);
+	
+	global $wpdb;
+	$affected = $wpdb->query("UPDATE $wpdb->postmeta SET meta_key='featured_tag' WHERE meta_key='feature_tag'");
+	/*
+	$affected = $wpdb->update(
+		"wp_postmeta",
+		array("meta_key"	=>	"featured_tag"),
+		array("meta_key"	=>	"feature_tag"),
+		array("%s"),
+		array("%s")
+		);
+	*/
+	
+	
+	
+	printf("<br /><strong>%d hidden posts fixed</strong>",
+		$affected
 	);
 	
 }
