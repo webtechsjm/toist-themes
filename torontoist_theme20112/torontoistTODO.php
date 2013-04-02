86a9aeba805bb40e6d88a33903947722ac9f47cd
Senning Luk <sluk@stage.torontoist.stjosephmedia.com>
2013-04-02 13:15:01 +0000

    TODO switch to template

--- torontoist_theme20112/torontoistTODO.php
+++ torontoist_theme20112/torontoistTODO.php
@@ -26,95 +26,14 @@ $(function() {
 
 </script>
 
-<div id="todo_head" style="margin:20px 0 40px 25px;">
-  <img src="http://media.torontolife.com/img/TOdo/TOdo_header_noSponsor.gif" border="0" usemap="#todo_sponsor" />
-  <map name="todo_sponsor" id="todo_sponsor">
-    <area shape="rect" coords="469,4,598,25" href="" target="_blank" alt="sponsor link" />
-    <area shape="rect" coords="460,46,598,65" href="http://www.torontolife.com/TOdo_sales/" alt="Add Your Event" />
-  </map>
-</div>
-<!--Mesh Marketing-->
-<div class="todo_container"><img src="http://media.torontolife.com/img/TOdo/mesh_600x100.jpg" alt="Mesh" width="600px" height="100px" />
-<p class="expand" style="margin-top: 5px; margin-bottom: 10px;">Details</p>
-
-<div class="collapse"><img class="product" src="http://media.torontolife.com/img/TOdo/mesh_260x260.jpg" alt="Mesh" width="260px" height="260px" />
-<div class="info">
-
-<strong>Canada's premier digital conference</strong> is a chance to connect with people who are as excited about the potential of digital as you are — people who want to know more about how it is changing the way we live, work and interact with the world.
-
-<strong><a style="color: #000;" href="http://www.torontolife.com/meshmarketing_todo_torontoist" rel="nofollow" target="_blank">Click here for more info »</a></strong>
-
-</div>
-<img src="http://media.torontolife.com/hotdeals/images/underline.gif" alt="" width="600px" height="20px" />
-
-</div>
-</div>
-<!--end Mesh Marketing-->
-<!--Images Festival-->
-<div class="todo_container"><img src="http://media.torontolife.com/img/TOdo/IF2013_600x100.jpg" alt="" width="600px" height="100px" />
-<p class="expand" style="margin-top: 5px; margin-bottom: 10px;">Details</p>
-
-<div class="collapse"><img class="product" src="http://media.torontolife.com/img/TOdo/IF2013_260x260.jpg" alt="Images Festival" width="260px" height="260px" />
-<div class="info">
-
-<strong>The Images Festival is Toronto's original inter-disciplinary arts festival.</strong>
-"Expanding cinema" since 1988, the Images Festival celebrates the moving image in all its forms – going beyond theatrical screenings to embrace live performances, gallery installations and site-specific interventions. The 2013 Images Festival takes place April 11-20 at 20 venues across the GTA!
-
-<strong><a style="color: #000;" href="http://www.torontolife.com/imagesfestival_todo" rel="nofollow" target="_blank">Click here for more info »</a></strong>
-
-</div>
-<img src="http://media.torontolife.com/hotdeals/images/underline.gif" alt="" width="600px" height="20px" />
-
-</div>
-</div>
-<!--end Images Festival-->
-<!--Cottage Life-->
-<div class="todo_container"><img src="http://media.torontolife.com/img/TOdo/cottage-life-600x100.jpg" alt="" width="600px" height="100px" />
-<p class="expand" style="margin-top: 5px; margin-bottom: 10px;">Details</p>
-
-<div class="collapse"><img class="product" src="http://media.torontolife.com/img/TOdo/cottage-life-260x260.jpg" alt="" width="260px" height="260px" />
-<div class="info">
-
-<strong>Summer starts now!</strong>
-
-Kick off cottage season with a visit to the 20th annual Spring Cottage Life Show. Visit our 500+ exhibitors to find boats, docks, cottage builders, innovative building products, green solutions, decor, food, entertaining ideas, water toys and fun for all seasons!
-Join us April 5-7 at the International Centre, Mississauga, 6900 Airport Road.
-
-<strong><a style="color: #000;" href="http://www.torontolife.com/cottagelife_todo" rel="nofollow" target="_blank">Click here for more info »</a></strong>
-
-</div>
-<img src="http://media.torontolife.com/hotdeals/images/underline.gif" alt="" width="600px" height="20px" />
-
-</div>
-</div>
-<!--end Cottage Life-->
-<!--ballet jorgen-->
-<div class="todo_container"><img src="http://media.torontolife.com/img/TOdo/swan-lake_600x100.jpg" alt="" width="600px" height="100px" />
-<p class="expand" style="margin-top: 5px; margin-bottom: 10px;">Details</p>
-
-<div class="collapse"><img class="product" src="http://media.torontolife.com/img/TOdo/swan-lake_260x260.jpg" alt="" width="260px" height="260px" />
-<div class="info">
-
-<strong>SWAN LAKE</strong>
-
-<strong>March 27, 28, &amp; 30, 2013</strong>
-
-<strong>Betty Oliphant Theatre</strong>
-
-<strong>Tickets: 416.978.8849 or </strong><strong><a style="color: #000;" href="http://www.uofttix.ca" rel="nofollow" target="_blank">www.uofttix.ca</a></strong>
-
-Ballet Jörgen Canada presents a spectacular new production of Swan Lake. Choreographed by Bengt Jörgen, this newly-imagined version of the iconic ballet is set at the Fortress of Louisbourg, Nova Scotia.
-
-Tickets $29 - $75.
-
-<strong><a style="color: #000;" href="http://www.torontolife.com/balletjorgencanada_swanlake_todo" rel="nofollow" target="_blank">Click here for more info »</a></strong>
-
-</div>
-<img src="http://media.torontolife.com/hotdeals/images/underline.gif" alt="" width="600px" height="20px" />
-
-</div>
-</div>
-<!--end ballet jorgen-->
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<div class="post" id="post-<?php the_ID(); ?>">
+	<h2><?php //the_title(); ?></h2>
+		<div class="entry">
+			<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
+		</div> 
+	</div>
+<?php endwhile; endif; ?>
 </div>
 
 <?php get_sidebar(); ?>
